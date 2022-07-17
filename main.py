48685ccfaef66558e75c38e4f0ffe592fec18661
Jhon Diaz <diazj691@gmail.com>
2022-07-17 11:19:51 -0500

    template(dashboard) integrado

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 from flask import Flask, render_template, request, redirect, url_for
+import re
 
 app = Flask(__name__)
 
-#ruta
+#rutas html
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
-def login():
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/curso')
+def curso():
+    return render_template('curso.html')
+
+@app.route('/estudiante')
+def estudiante():
+    return render_template('estudiante.html')
+
+
+#rutas validaciones
+@app.route('/iniciarSession', methods=['POST'])
+def iniciarSession():
     usuario = request.form.get('usuario')
 
     if usuario == 'jhondoe':
-        return f"Hola {usuario}"
+        return redirect(url_for('dashboard'))
     
     return redirect(url_for('index', error='error'))
 
-    
+# 0 = no existe. 1 = credenciales malas
+def mensajeError(error):
+    if (error != None and error != ''):
+            mensajes = ['El usuario ingresado no existe.', 'Credenciales ingresada incorrectas.']
+            error = int(error)
+
+            return mensajes[error]
+
+    return ''
 
 #para identificar que el archivo principal es el main.py
 if __name__ == '__main__':
